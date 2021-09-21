b582dce4480ca8a6a657b7caf62fb9cbe363cadb
MCMP18 <mcmp18@outlook.com>
2021-09-20 19:46:41 -0500

    Commit 1

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio
+{
+    public class Buses
+    {
+        public int id {set; get;}
+        public string marca {set; get;}
+        public int modelo {set; get;}
+        public int kilomatraje {set; get;}
+        public int numero_asientos {set; get;}
+        public string placa {set; get;}
+
+    }
+}
---
+++ ProyectoCiclo3.App.Frontend/Pages/Rutas/Create.cshtml.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+ 
+namespace ProyectoCiclo3.App.Frontend.Pages
+{
+    public class FormRutaModel : PageModel
+    {
+        public void OnGet()
+        {
+ 
+        }
+    }
+}
