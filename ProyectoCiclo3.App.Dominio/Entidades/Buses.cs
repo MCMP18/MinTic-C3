f21f7ffd4c7d69edfd8238298ae9238f659f7e3c
MCMP18 <mcmp18@outlook.com>
2021-09-26 19:07:31 -0500

    V5

--- ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -6,7 +6,7 @@ namespace ProyectoCiclo3.App.Dominio
         public int id {set; get;}
         public string marca {set; get;}
         public int modelo {set; get;}
-        public int kilomatraje {set; get;}
+        public int kilometraje {set; get;}
         public int numero_asientos {set; get;}
         public string placa {set; get;}
 
